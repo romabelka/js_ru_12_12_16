108ca1077a9405161fa7ccb1dc4344e16f0de1f5
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-12-15 18:19:15 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    }
+}
