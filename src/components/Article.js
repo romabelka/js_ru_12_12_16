eb33e763af2a36ad37118a92e597875bc460f746
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-12-19 20:03:15 +0200

    add accordion

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,44 +2,26 @@ import React, { Component, PropTypes } from 'react'
 import CommentList from './CommentList'
 
 export default class Article extends Component {
-    state = {
-        isOpen: false,
-    }
-
     static propTypes = {
         article: PropTypes.object.isRequired
     }
 
-/*
-    constructor() {
-        super()
-        this.state = {
-            isOpen: false
-        }
-    }
-*/
     componentDidMount() {
         console.log('---', this.refs.container)
     }
 
     render() {
-        const { article } = this.props
+        const { article, onClick } = this.props
         return (
             <div ref = "container">
-                <h3 onClick = {this.toggleOpen}>{article.title}</h3>
+                <h3 onClick = {onClick}>{article.title}</h3>
                 {this.getBody()}
             </div>
         )
     }
 
-    toggleOpen = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
-
     getBody() {
-        if (!this.state.isOpen) return null
+        if (!this.props.isOpen) return null
         return (
             <section>
                 {this.props.article.text}
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,20 +1,37 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 import Article from './Article'
 import Chart from './Chart'
 
-export default function ArticleList(props) {
-    const { articles } = props
-    const articleElements = articles.map(article => <li key = {article.id}><Article article = {article} /></li>)
-    return (
-        <div>
-            <h2>Article List</h2>
-            <ul>
-                {/*some comment*/}
-                {articleElements}
-            </ul>
-            <Chart articles = {articles}/>
-        </div>
-    )
+export default class ArticleList extends React.Component {
+    state = {
+        openArticleId: null
+    }
+    render() {
+        const {articles} = this.props
+        const articleElements = articles.map(article =>
+            <li key={article.id}>
+                <Article article={article}
+                         isOpen={this.state.openArticleId == article.id}
+                         onClick={this.toggleOpenArticle(article.id)}
+                />
+            </li>)
+        return (
+            <div>
+                <h2>Article List</h2>
+                <ul>
+                    {/*some comment*/}
+                    {articleElements}
+                </ul>
+                <Chart articles={articles}/>
+            </div>
+        )
+    }
+
+    toggleOpenArticle = id => ev => {
+        this.setState({
+            openArticleId: id
+        })
+    }
 }
 
 ArticleList.propTypes = {
